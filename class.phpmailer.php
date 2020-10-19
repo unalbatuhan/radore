c4f6a08483a91e1385e31db80d9d44a7dd277069
Batuhan UNAL <unal.batuhan@outlook.com>
2020-10-19 04:33:44 +0300

    Update class.phpmailer.php

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -12,7 +12,7 @@
 |          : Marcus Bointon (coolbru) coolbru@users.sourceforge.net         |
 |   Founder: Brent R. Matzelle (original founder)                           |
 | Copyright (c) 2004-2009, Andy Prevost. All Rights Reserved.               |
-| Copyright (c) 2001-2003, Brent R. Matzelle                                |
+|                               |
 | ------------------------------------------------------------------------- |
 |   License: Distributed under the Lesser General Public License (LGPL)     |
 |            http://www.gnu.org/copyleft/lesser.html                        |
